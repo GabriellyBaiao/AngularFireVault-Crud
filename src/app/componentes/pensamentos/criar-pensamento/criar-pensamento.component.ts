36865edd126c847fe52c0ea2690fb3ef3fbfe4a5
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-02-17 13:51:27 -0300

    [PTI] feat: implementing pagination

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,7 +1,6 @@
+import { Router } from '@angular/router';
+import { PensamentoService } from './../pensamento.service';
 import { Component, OnInit } from '@angular/core';
-import { Pensamento } from '../pensamento';
-import { PensamentoService } from '../pensamento.service';
-import { Route, Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -11,31 +10,29 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-
   formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
     private formBuilder: FormBuilder
-    ) { }
+  ) { }
 
   ngOnInit(): void {
     this.formulario = this.formBuilder.group({
       conteudo: ['', Validators.compose([
         Validators.required,
-        Validators.pattern(/(.|\s)*\S(.|\s)*/),
+        Validators.pattern(/(.|\s)*\S(.|\s)*/)
       ])],
-      autoria: ['', Validators.compose ([
+      autoria: ['', Validators.compose([
         Validators.required,
-        Validators.minLength(3),
+        Validators.minLength(3)
       ])],
       modelo: ['modelo1']
     })
   }
 
   criarPensamento() {
-    console.log(this.formulario.get('autoria')?.errors)
     if(this.formulario.valid){
       this.service.criar(this.formulario.value).subscribe(() => {
         this.router.navigate(['/listarPensamento'])
@@ -43,8 +40,16 @@ export class CriarPensamentoComponent implements OnInit {
     }
   }
 
-  cancelar(){
+  cancelar() {
     this.router.navigate(['/listarPensamento'])
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return 'botao'
+    } else {
+      return 'botao__desabilitado'
+    }
   }
 
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Pensamento } from './pensamento';
 import { Observable } from 'rxjs';
 
@@ -9,29 +9,38 @@ import { Observable } from 'rxjs';
 export class PensamentoService {
 
   private readonly API = 'http://localhost:3000/pensamentos'
+
   constructor(private http: HttpClient) { }
 
-    listar(): Observable<Pensamento[]> {
-      return this.http.get<Pensamento[]>(this.API)
-    }
+  listar(pagina: number): Observable<Pensamento[]> {
 
-    criar(pensamento: Pensamento): Observable<Pensamento>{
-      return this.http.post<Pensamento>(this.API, pensamento);
-    }
+    const itensPorPagina = 6;
 
-    editar(pensamento: Pensamento): Observable<Pensamento> {
-      const url = `${this.API}/${pensamento.id}`
-      return this.http.put<Pensamento>(url, pensamento)
-    }
+    let params = new HttpParams()
+      .set("_page", pagina)
+      .set("_limit", itensPorPagina)
 
-    excluir(id: string): Observable<Pensamento> {
-      const url = `${this.API}/${id}`
-      return this.http.delete<Pensamento>(url)
+    return this.http.get<Pensamento[]>(this.API, { params})
+  }
+
+  criar(pensamento: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, pensamento)
+  }
+
+  editar(pensamento: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${pensamento.id}`
+    return this.http.put<Pensamento>(url, pensamento )
+
+  }
+
+  excluir(id: string): Observable<Pensamento> {
+    const url = `${this.API}/${id}`
+    return this.http.delete<Pensamento>(url)
   }
 
   buscarPorId(id: string): Observable<Pensamento> {
     const url = `${this.API}/${id}`
     return this.http.get<Pensamento>(url)
   }
-}
 
+}
